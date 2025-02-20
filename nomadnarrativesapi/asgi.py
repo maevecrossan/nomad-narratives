34192d12ddcf43ecdaa756bd837dfe41c0f7e819
Maeve Crossan <maevecrossan@gmail.com>
2025-02-20 09:35:02 +0000

    Re create django app to fix naming error. Update Pillow version to be compatible with Python 3.12. Create Profiles model and add to admin panel.

--- nomadnarrativesapi/asgi.py
+++ nomadnarrativesapi/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for nomadnarratives project.
+ASGI config for nomadnarrativesapi project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nomadnarratives.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nomadnarrativesapi.settings')
 
 application = get_asgi_application()
--- nomadnarrativesapi/wsgi.py
+++ nomadnarrativesapi/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for nomadnarratives project.
+WSGI config for nomadnarrativesapi project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nomadnarratives.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nomadnarrativesapi.settings')
 
 application = get_wsgi_application()
