28ce8b3bdc647326355d239b9c90416268d8e9b5
Maeve Crossan <maevecrossan@gmail.com>
2025-02-20 11:07:13 +0000

    Create Profile detail view and add docstrings.

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,8 @@
 Profile Views
 '''
 
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -10,6 +12,7 @@ from .serializers import ProfileSerializer
 class ProfileList(APIView):
     '''
     Returns a list of all profiles.
+    No Create view (profile creation handled by Django signals).
     '''
     def get(self, request):
         '''
@@ -18,3 +21,38 @@ class ProfileList(APIView):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    '''
+    Allows profile retrieval and updating.
+    '''
+    serializer_class = ProfileSerializer
+    def get_object(self, pk):
+        '''
+        Retrieves profiles.
+        '''
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        '''
+        Retrieves profile based on ID.
+        '''
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        '''
+        Updates retrieved profile based on ID.
+        '''
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
