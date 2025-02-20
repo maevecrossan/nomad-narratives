e35416e56994e7f3da1c48a57c55f4d005e11a1f
Maeve Crossan <maevecrossan@gmail.com>
2025-02-20 13:14:03 +0000

    Add post editing and deleting functionality.

--- profiles/views.py
+++ profiles/views.py
@@ -6,9 +6,10 @@ from django.http import Http404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from nomadnarrativesapi.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
-from nomadnarrativesapi.permissions import IsOwnerOrReadOnly
+
 
 class ProfileList(APIView):
     '''
