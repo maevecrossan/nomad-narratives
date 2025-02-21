ca150ec38c2eeca6a0a9f6e895227f4c988112f4
Maeve Crossan <maevecrossan@gmail.com>
2025-02-21 13:08:43 +0000

    Created comments serializers and views. Added urls. Changes migrated to database.

--- comments/models.py
+++ comments/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from posts.models import Post
+from posts.models import TripPost
 
 
 class Comment(models.Model):
@@ -8,7 +8,7 @@ class Comment(models.Model):
     Comment model, related to User and Post
     '''
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(TripPost, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
---
+++ comments/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    '''
+    Serializer for Comment model.
+    '''
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def get_is_owner(self, obj):
+        '''
+        Checks for owner of comment.
+        '''
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        '''
+        Specifies what fields to be returned.
+        '''
+        model = Comment
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'post', 'created_at',
+            'updated_at', 'content']
+
+
+class CommentDetailSerializer(CommentSerializer):
+    '''
+    Serializer for the Comment model used in Detail view.
+    '''
+    post = serializers.ReadOnlyField(source='post.id')
