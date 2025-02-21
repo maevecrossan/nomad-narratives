75e6acee5098c930d36a88c91d023914ff7f227d
Maeve Crossan <maevecrossan@gmail.com>
2025-02-21 10:06:21 +0000

    Adjust admin display to remove subregion category. Remigrate cities-light.

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from cities_light.models import City
 
-# Register your models here.
+
+class CityAdmin(admin.ModelAdmin):
+    '''
+    Cleans up admin panel to remove unnecessary additonal fields.
+    '''
+    list_display = ['name']  # Only display the city name
+    search_fields = ['name']
+
+
+admin.site.unregister(City)
+admin.site.register(City, CityAdmin)
