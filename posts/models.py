cd7cd36e873cf77e2752a0e97970d5cd0737e58b
Maeve Crossan <maevecrossan@gmail.com>
2025-02-21 09:10:06 +0000

    Remove unused 'region' import.

--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,7 @@
 '''
 
 from django.db import models
-from cities_light.models import City, Region, Country
+from cities_light.models import City, Country
 from django.contrib.auth.models import User
 from utils.continents import get_continent_by_country
 
