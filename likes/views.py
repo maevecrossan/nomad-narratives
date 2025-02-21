66e367323a144e3cbb89f666ea48c1a890742c7b
Maeve Crossan <maevecrossan@gmail.com>
2025-02-21 14:04:17 +0000

    Add generic LikeList view.

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Like
+from .serializers import LikeSerializer
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    '''
+    Lists all likes.
+    Creates a like if user is authenticated.
+    The perform_create method links the like with the logged in user.
+    '''
+
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
