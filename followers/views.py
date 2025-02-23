d086a6ea054cc153fce33129357c7d9f9ecb1393
Maeve Crossan <maevecrossan@gmail.com>
2025-02-23 13:36:42 +0000

    Create FollowerList and FollowerDetail views.py

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Follower
+from nomadnarrativesapi.permissions import IsOwnerOrReadOnly
+from .serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    '''
+    Retrieves a follower.
+    Allows users to follow or unfollow another user.
+    '''
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+    permission_classes = [IsOwnerOrReadOnly]
