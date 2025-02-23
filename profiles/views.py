cb32860cd518eb8ebedc83e5fb87fb6cae21c1fd
Maeve Crossan <maevecrossan@gmail.com>
2025-02-23 13:44:00 +0000

    Refactor profiles/views.py.

--- profiles/views.py
+++ profiles/views.py
@@ -2,68 +2,25 @@
 Profile Views
 '''
 
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
 from nomadnarrativesapi.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
-class ProfileList(APIView):
+class ProfileList(generics.ListAPIView):
     '''
     Returns a list of all profiles.
     No Create view (profile creation handled by Django signals).
     '''
-    def get(self, request):
-        '''
-        Retrieves all profiles and returns them in a response.
-        '''
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles, many=True, context={'request': request}
-            )
-        return Response(serializer.data)
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
 
 
-class ProfileDetail(APIView):
+class ProfileDetail(generics.RetrieveUpdateAPIView):
     '''
     Allows profile retrieval and updating.
     '''
     serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        '''
-        Retrieves profiles.
-        '''
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        '''
-        Retrieves profile based on ID.
-        '''
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, context={'request': request}
-            )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        '''
-        Updates retrieved profile based on ID.
-        '''
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, data=request.data, context={'request': request}
-            )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
