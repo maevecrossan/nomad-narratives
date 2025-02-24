298237fd451e2947285f7062cc34e6356dc41f3e
Maeve Crossan <maevecrossan@gmail.com>
2025-02-24 17:02:02 +0000

    Add spacing to make PEP8 compliant.

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
+
 class Profile(models.Model):
     '''
     Profile model.
@@ -24,11 +25,13 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.owner}'s profile"
 
+
 def create_profile(sender, instance, created, **kwargs):
     '''
     Creates a new profile every time a user is created.
     '''
     if created:
         Profile.objects.create(owner=instance)
-    
+
+
 post_save.connect(create_profile, sender=User)
