512556468e3739cfb17f2f88efcc4f9ab72f56c1
Maeve Crossan <maevecrossan@gmail.com>
2025-02-24 17:44:29 +0000

    Add filter field to show comments belonging to a certain post.

--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from nomadnarrativesapi.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
@@ -14,6 +15,13 @@ class CommentList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
+
+    filter_backends = [
+        DjangoFilterBackend
+        ]
+    filterset_fields = [
+        'trippost'
+        ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
