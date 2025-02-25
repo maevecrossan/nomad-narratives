e32a5a29cb31d6d1348c95ff9a4bb93833404ba1
Maeve Crossan <maevecrossan@gmail.com>
2025-02-25 14:02:00 +0000

    Fix alt text field serializer typo and add to fields.

--- posts/serializers.py
+++ posts/serializers.py
@@ -51,7 +51,7 @@ class TripPostSerializer(serializers.ModelSerializer):
     likes_count = serializers.ReadOnlyField()
     comments_count = serializers.ReadOnlyField()
     content = serializers.CharField()
-    alt_text = serializers.CharField(
+    image_alt_text = serializers.CharField(
         required=True,
         allow_blank=False)
 
@@ -138,6 +138,6 @@ class TripPostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'image_filter', 'like_id',
-            'likes_count', 'comments_count', 'details'
+            'title', 'content', 'image', 'image_filter', 'image_alt_text',
+            'like_id', 'likes_count', 'comments_count', 'details'
         ]
