3339f5823f5cea9210065039e74ff3210356474d
Maeve Crossan <maevecrossan@gmail.com>
2025-03-04 17:52:48 +0000

    Update user serializer file path.

--- nomadnarrativesapi/settings.py
+++ nomadnarrativesapi/settings.py
@@ -91,7 +91,8 @@ JWT_AUTH_SECURE = True
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 
 REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
+    'USER_DETAILS_SERIALIZER': 'nomadnarrativesapi.serializers.\
+        CurrentUserSerializer'
     }
 
 CITIES_LIGHT_TRANSLATION_LANGUAGES = ['en']
