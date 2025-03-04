e150d0251fc6c47eb9e73db09b48284cc2910ce9
Maeve Crossan <maevecrossan@gmail.com>
2025-03-04 14:41:03 +0000

    Add missing docstrings.

--- followers/urls.py
+++ followers/urls.py
@@ -1,3 +1,9 @@
+'''
+Defines URL patterns for managing follower relationships, 
+including listing followers and viewing details of a specific 
+follower relationship.
+'''
+
 from django.urls import path
 from followers import views
 
