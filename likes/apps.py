b06c9e2f3d6de79884212719f20191be2883454d
Maeve Crossan <maevecrossan@gmail.com>
2025-03-04 14:45:53 +0000

    Reorganise imports and add missing docstrings.

--- likes/apps.py
+++ likes/apps.py
@@ -1,6 +1,13 @@
+'''
+Configures the "likes" app, defining the default auto field type and app name.
+'''
 from django.apps import AppConfig
 
 
 class LikesConfig(AppConfig):
+    '''
+    Configuration for the "likes" app, specifying the default auto field type
+    and the app name.
+    '''
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'likes'
