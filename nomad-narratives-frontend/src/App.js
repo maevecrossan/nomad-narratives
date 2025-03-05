0ed5a8b8d82a2bb351969c9364ff74b2bb533b31
Maeve Crossan <maevecrossan@gmail.com>
2025-03-05 19:18:34 +0000

    Import NavBar component and bootstrap css.

--- nomad-narratives-frontend/src/App.js
+++ nomad-narratives-frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar';
 
 function App() {
 return (
 	<div className="App">
-		<h1>Hello, world!</h1>
+		< NavBar />
 	</div>
 );
 }
