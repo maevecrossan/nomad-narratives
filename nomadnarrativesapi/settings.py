8b80e65e59e36d70030de1a5230e529acfe937f8
Maeve Crossan <maevecrossan@gmail.com>
2025-03-05 09:45:58 +0000

    Update allowed hosts.

--- nomadnarrativesapi/settings.py
+++ nomadnarrativesapi/settings.py
@@ -40,7 +40,8 @@ DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = [
     'localhost',
-    '0.0.0.0',
+    '127.0.0.1',
+    'https://nomad-narratives-05968209a16d.herokuapp.com/',
 ]
 
 
