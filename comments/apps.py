377cff4901b3359b931a4ec10c7c3d43d5412d70
Maeve Crossan <maevecrossan@gmail.com>
2025-03-08 10:56:17 +0000

    Add missing docstrings.

--- comments/apps.py
+++ comments/apps.py
@@ -1,6 +1,15 @@
+'''
+App configuration for the Comments application.
+
+This module defines the configuration settings for the Comments app
+within the Django project.
+'''
 from django.apps import AppConfig
 
 
 class CommentsConfig(AppConfig):
+    '''
+    This class sets the default primary key type and specifies the app name.
+    '''
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'comments'
