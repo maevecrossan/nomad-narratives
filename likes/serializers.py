b747b9cdda2e7eddd5cf3e4e6f68f2d32db0b61f
Maeve Crossan <maevecrossan@gmail.com>
2025-03-08 11:44:28 +0000

    Explicitly re-raise integrity error using the 'from exc' syntax.

--- likes/serializers.py
+++ likes/serializers.py
@@ -27,5 +27,6 @@ class LikeSerializer(serializers.ModelSerializer):
         '''
         try:
             return super().create(validated_data)
-        except IntegrityError:
-            raise serializers.ValidationError({'detail': 'possible duplicate'})
+        except IntegrityError as exc:
+            raise serializers.ValidationError(
+                {'detail': 'possible duplicate'}) from exc
