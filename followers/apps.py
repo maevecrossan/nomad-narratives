6b8d328eb93c50ca27d491afd0aa8df3a462c211
Maeve Crossan <maevecrossan@gmail.com>
2025-03-08 11:23:58 +0000

    Add missing docstrings.

--- followers/apps.py
+++ followers/apps.py
@@ -1,6 +1,17 @@
+'''
+App configuration for the Followers application.
+
+This module defines the configuration settings for the Followers app
+within the Django project.
+'''
 from django.apps import AppConfig
 
 
 class FollowersConfig(AppConfig):
+    '''
+    Configuration class for the Followers app.
+
+    This class sets the default primary key type and specifies the app name.
+    '''
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'followers'
