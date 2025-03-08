68df25a088a685f042c72ff67c01fc098ba73a36
Maeve Crossan <maevecrossan@gmail.com>
2025-03-08 14:54:18 +0000

    Add missing docstring.

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,3 +1,16 @@
+'''
+Defines URL routes for accessing and interacting with user profiles.
+
+- `ProfileList`: View for listing all profiles and creating new profiles.
+- `ProfileDetail`: View for retrieving, updating, and
+   deleting a specific profile.
+
+URLs:
+- `/profiles/`: List all profiles or create a new one.
+- `/profiles/<int:pk>/`: Retrieve, update, or delete a profile by
+   its primary key.
+'''
+
 from django.urls import path
 from profiles import views
 
