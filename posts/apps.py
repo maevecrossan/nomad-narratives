c6c7f2681e40369a4edc170e86ec1af281f74e91
Maeve Crossan <maevecrossan@gmail.com>
2025-03-08 11:58:51 +0000

    Add docstrings.

--- posts/apps.py
+++ posts/apps.py
@@ -1,6 +1,15 @@
+'''
+App configuration for the Posts application.
+
+This module defines the configuration settings for the Posts app
+within the Django project.
+'''
 from django.apps import AppConfig
 
 
 class PostsConfig(AppConfig):
+    '''
+    This class sets the default primary key type and specifies the app name.
+    '''
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'posts'
