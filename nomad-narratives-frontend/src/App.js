f36c26e3a1030f49c840cee37ea8f1c923353617
Maeve Crossan <maevecrossan@gmail.com>
2025-03-09 14:43:55 +0000

    Format route paths to be more readable.

--- nomad-narratives-frontend/src/App.js
+++ nomad-narratives-frontend/src/App.js
@@ -10,14 +10,44 @@ return (
 		< NavBar />
 		<Container className={styles.Main}>
 			<Switch>
-				<Route exact path="/welcome" render={() => <h1>Welcome</h1>} />
-				<Route exact path="/myfeed" render={() => <h1>My Feed</h1>} />
-				<Route exact path="/myprofile" render={() => <h1>My Profile</h1>} />
-				<Route exact path="/logout" render={() => <h1>Log Out</h1>} />
-				<Route exact path="/aboutus" render={() => <h1>About Us</h1>} />
-				<Route exact path="/communityguidelines" render={() => <h1>Community Guidelines</h1>} />
-				<Route exact path="/signin" render={() => <h1>Sign In</h1>} />
-				<Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+				<Route 
+					exact 
+					path="/welcome" 
+					render={() => <h1>Welcome</h1>} />
+
+				<Route 
+					exact 
+					path="/myfeed" 
+					render={() => <h1>My Feed</h1>} />
+
+				<Route 
+					exact 
+					path="/myprofile" 
+					render={() => <h1>My Profile</h1>} />
+
+				<Route 
+					exact 
+					path="/logout" 
+					render={() => <h1>Log Out</h1>} />
+				
+				<Route 
+					exact 
+					path="/aboutus" 
+					render={() => <h1>About Us</h1>} />
+				
+				<Route 
+					exact 
+					path="/communityguidelines" 
+					render={() => <h1>Community Guidelines</h1>} />
+
+				<Route 
+					exact 
+					path="/signin" render={() => <h1>Sign In</h1>} />
+
+				<Route 
+					exact 
+					path="/signup" 
+					render={() => <h1>Sign Up</h1>} />
 			</Switch>
 		</Container>
 	</div>
