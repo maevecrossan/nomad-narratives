7dcc22a539058bdbc2dd36209d76ef746a09e4cd
Maeve Crossan <maevecrossan@gmail.com>
2025-03-09 16:13:44 +0000

    Comment out explore dropdown routes to use in a future iteration.

--- nomad-narratives-frontend/src/App.js
+++ nomad-narratives-frontend/src/App.js
@@ -17,36 +17,59 @@ return (
 
 				<Route 
 					exact 
-					path="/myfeed" 
+					path="/my-feed" 
 					render={() => <h1>My Feed</h1>} />
 
 				<Route 
 					exact 
-					path="/myprofile" 
+					path="/my-profile" 
 					render={() => <h1>My Profile</h1>} />
+
+				{/* FUTURE DEVELOPMENT: Explore Dropdown */}
+				{/* <Route 
+					exact 
+					path="/by-region" 
+					render={() => <h1>Region</h1>} />
 
 				<Route 
 					exact 
-					path="/logout" 
+					path="/by-country" 
+					render={() => <h1>Country</h1>} />
+				<Route 
+					exact 
+					path="/by-city" 
+					render={() => <h1>City</h1>} />
+				<Route 
+					exact 
+					path="/by-target-audience" 
+					render={() => <h1>Target Audience</h1>} />
+				<Route 
+					exact 
+					path="/by-duration" 
+					render={() => <h1>Duration</h1>} /> */}
+
+				<Route 
+					exact 
+					path="/log-out" 
 					render={() => <h1>Log Out</h1>} />
 				
 				<Route 
 					exact 
-					path="/aboutus" 
+					path="/about-us" 
 					render={() => <h1>About Us</h1>} />
 				
 				<Route 
 					exact 
-					path="/communityguidelines" 
+					path="/community-guidelines" 
 					render={() => <h1>Community Guidelines</h1>} />
 
 				<Route 
 					exact 
-					path="/signin" render={() => <h1>Sign In</h1>} />
+					path="/sign-in" render={() => <h1>Sign In</h1>} />
 
 				<Route 
 					exact 
-					path="/signup" 
+					path="/sign-up" 
 					render={() => <h1>Sign Up</h1>} />
 			</Switch>
 		</Container>
