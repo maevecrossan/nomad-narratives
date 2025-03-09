d20628075579a1e34da9ebff833c3210a97a8282
Maeve Crossan <maevecrossan@gmail.com>
2025-03-09 14:15:15 +0000

    Create 'main' class to add padding to the top of the container.

--- nomad-narratives-frontend/src/App.js
+++ nomad-narratives-frontend/src/App.js
@@ -1,11 +1,17 @@
 import styles from './App.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
+import { Container } from 'react-bootstrap';
 
 function App() {
 return (
 	<div className={styles.App}>
 		< NavBar />
+		<Container className={styles.Main}>
+			<h1>Home</h1>
+			<h1>Sign In</h1>
+			<h1>Sign Up</h1>
+		</Container>
 	</div>
 );
 }
