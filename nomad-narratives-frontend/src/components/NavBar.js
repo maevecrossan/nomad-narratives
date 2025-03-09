2b2b6a2a84fe4f32108b38e69dd7f92b71f68985
Maeve Crossan <maevecrossan@gmail.com>
2025-03-09 14:41:50 +0000

    Reformat links to use react router NavLink and add 'to' attribute to wire up routes.

--- nomad-narratives-frontend/src/components/NavBar.js
+++ nomad-narratives-frontend/src/components/NavBar.js
@@ -2,30 +2,38 @@ import React from 'react'
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import logo from '../assets/nn-logo-transparent.png'
 import styles from '../styles/NavBar.module.css'
+import { NavLink } from 'react-router-dom/cjs/react-router-dom';
 
 // NavBar for Logged In User
 const NavBar = () => {
     return (
         <Navbar className={styles.NavBar} expand="md" fixed='top'>
-            <Navbar.Brand href="#home">
-                <img src={logo} alt="Logo" className={styles.navbarLogo}/>
-            </Navbar.Brand>
+            <NavLink to="/">
+                <Navbar.Brand>
+                    <img src={logo} 
+                    alt="Logo" 
+                    className={styles.navbarLogo}
+                />
+                </Navbar.Brand>
+            </NavLink>
+
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto text-center">
                 
                 {/* Logged In State */}
-                <Nav.Link>
+                <NavLink  to="/myfeed">
                     <i className='fas fa-home'></i>
                     My Feed
-                </Nav.Link>
-                <Nav.Link>
+                </NavLink>
+
+                <NavLink to="/myprofile">
                     <i class="fa-regular fa-circle-user"></i>
                     My Profile
-                </Nav.Link>
+                </NavLink>
 
                 {/* Explore Dropdown */}
-                <NavDropdown title={
+                {/* <NavDropdown title={
                     <>
                         <i className="fas fa-compass me-2"></i> Explore
                     </>
@@ -37,39 +45,43 @@ const NavBar = () => {
                     <NavDropdown.Item href="#action/3.5">Duration</NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
+                </NavDropdown> */}
 
                 {/* Help Dropdown */}
-                <NavDropdown title={
-                    <>
-                        <i className="fas fa-question"></i> Help
-                    </>
-                    } id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">About Us</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Community Guidelines</NavDropdown.Item>
-                </NavDropdown>
+                    {/* <NavDropdown title={
+                        <>
+                            <i className="fas fa-question"></i> Help
+                        </>
+                        } id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">About Us</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Community Guidelines</NavDropdown.Item>
+                    </NavDropdown> */}
 
-                <Nav.Link>
+                <NavLink to="/logout">
                     <i class="fa-solid fa-arrow-right-from-bracket"></i>
                     Log Out
-                </Nav.Link>
+                </NavLink>
                 
                 {/* Logged Out State */}
-                <Nav.Link href="#link">
+                <NavLink to="/aboutus">
                     <i class="fa-solid fa-info"></i>
                     About Us
-                </Nav.Link>
-                <Nav.Link href="#link">
+                </NavLink>
+
+                <NavLink to="/communityguidelines">
                     <i class="fa-regular fa-file-lines"></i>
-                    Community Guidelines</Nav.Link>
-                <Nav.Link>
+                    Community Guidelines
+                </NavLink>
+
+                <NavLink to="/signin">
                     <i class="fa-solid fa-sign-in-alt"></i>
                     Sign In
-                </Nav.Link>
-                <Nav.Link>
+                </NavLink>
+
+                <NavLink to="/signup">
                     <i class="fa-solid fa-user-plus"></i>
                     Sign up
-                </Nav.Link>
+                </NavLink>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
