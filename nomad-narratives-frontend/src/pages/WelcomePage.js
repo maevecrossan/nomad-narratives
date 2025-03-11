9d3a101041c485fc4a300be24ed16cc22a72d96f
Maeve Crossan <maevecrossan@gmail.com>
2025-03-11 07:37:09 +0000

    Create basic hero image and greeting layout. Add styles references to the row elements.

--- nomad-narratives-frontend/src/pages/WelcomePage.js
+++ nomad-narratives-frontend/src/pages/WelcomePage.js
@@ -1,27 +1,31 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import styles from '../styles/WelcomePage.module.css'
 
 const WelcomePage = () => {
     return (
         <Container fluid>
-            <Row>
-                <Col> Greeting / hero </Col>
+            <Row className={styles.Row}>
+                <Col className={styles.Hero}>
+                    <h1> 
+                        Welcome to <span>Nomad Narratives</span>
+                    </h1>
+                </Col>
             </Row>
 
-            <Row>
+            <Row className={styles.Row}>
                 <Col> About Us Graphic </Col>
                 <Col> About Us Statement </Col>
             </Row>
             
-            <Row>
+            <Row className={styles.Row}>
                 <Col> Guidelines Header </Col>
             </Row>
-            <Row>
+            <Row className={styles.Row}>
                 <Col> Guidelines info </Col>
             </Row>
         </Container>
-
     );
 }
 
