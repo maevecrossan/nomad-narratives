f498a613741aa4d2e88f71743370924a03eb73db
Maeve Crossan <maevecrossan@gmail.com>
2025-03-11 07:56:18 +0000

    Gives relevant columns the appropriate style references (text or image). Add images.

--- nomad-narratives-frontend/src/pages/WelcomePage.js
+++ nomad-narratives-frontend/src/pages/WelcomePage.js
@@ -15,15 +15,30 @@ const WelcomePage = () => {
             </Row>
 
             <Row className={styles.Row}>
-                <Col> About Us Graphic </Col>
-                <Col> About Us Statement </Col>
+                <Col className={styles.ImageCol}> 
+                    <img className={styles.ImageCol} src='https://res.cloudinary.com/dimeyes2b/image/upload/v1741679079/home-image-1_qcmr5e.jpg' alt='Waves crashing again rough, black volcanic rocks.'></img>
+                </Col>
+                
+                <Col className={styles.TextCol}> 
+                About Us Statement 
+                </Col>
             </Row>
             
             <Row className={styles.Row}>
-                <Col> Guidelines Header </Col>
+                <Col className={styles.ImageCol}> 
+                    <img className={styles.ImageCol} src='https://res.cloudinary.com/dimeyes2b/image/upload/v1741679510/126A7896_by84oj.jpg' alt='A korean temple as seenthrough orange and yellow autumn leaves.'></img> 
+                </Col>
+                <Col className={styles.ImageCol}> 
+                    <img className={styles.ImageCol} src='https://res.cloudinary.com/dimeyes2b/image/upload/v1741679624/126A0948_fb0ij6.jpg' alt='A picnic setup in front of the ocean.'></img> 
+                </Col>
+                <Col className={styles.ImageCol}> 
+                    <img className={styles.ImageCol} src='https://res.cloudinary.com/dimeyes2b/image/upload/v1741679620/126A2776_avm2j4.jpg' alt='A yellow front door in Hobbiton.'></img> 
+                </Col>
             </Row>
             <Row className={styles.Row}>
-                <Col> Guidelines info </Col>
+                <Col className={styles.TextCol}> 
+                Guidelines info 
+                </Col>
             </Row>
         </Container>
     );
