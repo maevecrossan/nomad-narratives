fd938387150d03ac52e7fafa1919ee29e64d43dc
Maeve Crossan <maevecrossan@gmail.com>
2025-03-11 08:13:32 +0000

    Remove gallery row and save for later iteration.

--- nomad-narratives-frontend/src/pages/WelcomePage.js
+++ nomad-narratives-frontend/src/pages/WelcomePage.js
@@ -20,24 +20,13 @@ const WelcomePage = () => {
                 </Col>
                 
                 <Col className={styles.TextCol}> 
-                About Us Statement 
+                    <h2>About Us</h2> 
                 </Col>
             </Row>
             
             <Row className={styles.Row}>
-                <Col className={styles.ImageCol}> 
-                    <img className={styles.ImageCol} src='https://res.cloudinary.com/dimeyes2b/image/upload/v1741679510/126A7896_by84oj.jpg' alt='A korean temple as seenthrough orange and yellow autumn leaves.'></img> 
-                </Col>
-                <Col className={styles.ImageCol}> 
-                    <img className={styles.ImageCol} src='https://res.cloudinary.com/dimeyes2b/image/upload/v1741679624/126A0948_fb0ij6.jpg' alt='A picnic setup in front of the ocean.'></img> 
-                </Col>
-                <Col className={styles.ImageCol}> 
-                    <img className={styles.ImageCol} src='https://res.cloudinary.com/dimeyes2b/image/upload/v1741679620/126A2776_avm2j4.jpg' alt='A yellow front door in Hobbiton.'></img> 
-                </Col>
-            </Row>
-            <Row className={styles.Row}>
                 <Col className={styles.TextCol}> 
-                Guidelines info 
+                    <h2>Community Guidelines</h2>
                 </Col>
             </Row>
         </Container>
