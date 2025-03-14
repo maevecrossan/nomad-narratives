78592e715de98b28182da0fab0698b5cd5b57b0a
Maeve Crossan <maevecrossan@gmail.com>
2025-03-14 10:01:32 +0000

    Import currentUserProvider and wrap around app.

--- nomad-narratives-frontend/src/index.js
+++ nomad-narratives-frontend/src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from 'react-router-dom';
+import { CurrentUserProvider } from './contexts/CurrentUserContext';
 
 ReactDOM.render(
-    <React.StrictMode>
+	<React.StrictMode>
 		<Router>
-      		<App />
+			<CurrentUserProvider>
+				<App />
+			</CurrentUserProvider>
 		</Router>
-    </React.StrictMode>,
-    document.getElementById('root')
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
