da7bd0d3ded3dd0459b41a3194a3c70401137723
Maeve Crossan <maevecrossan@gmail.com>
2025-03-14 09:21:01 +0000

    Add providers to user contenxts and wrap around the return div to expose to children.

--- nomad-narratives-frontend/src/App.js
+++ nomad-narratives-frontend/src/App.js
@@ -7,7 +7,11 @@ import './api/axiosDefaults';
 import SignUpForm from './pages/auth/SignUpForm';
 import WelcomePage from './pages/WelcomePage';
 import SignInForm from './pages/auth/SignInForm';
-import { useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+export const CurrentUserContext = createContext()
+export const SetCurrentUserContext = createContext()
 
 function App() {
 	const [currentUser, setCurrentUser] = useState(null)
@@ -26,79 +30,84 @@ function App() {
 	}, [])
 
 	return (
-		<div className={styles.App}>
-			< NavBar />
-			<Container className={styles.Main}>
-				<Switch>
-					<Route 
-						exact 
-						path="/welcome" 
-						render={() => <WelcomePage/>} />
+		<CurrentUserContext.Provider value={currentUser}>
+			<SetCurrentUserContext.Provider  value={setCurrentUser}>
 
-					<Route 
-						exact 
-						path="/my-feed" 
-						render={() => <h1>My Feed</h1>} />
+				<div className={styles.App}>
+					< NavBar />
+					<Container className={styles.Main}>
+						<Switch>
+							<Route 
+								exact 
+								path="/welcome" 
+								render={() => <WelcomePage/>} />
 
-					<Route 
-						exact 
-						path="/my-profile" 
-						render={() => <h1>My Profile</h1>} />
+							<Route 
+								exact 
+								path="/my-feed" 
+								render={() => <h1>My Feed</h1>} />
 
-					<Route 
-						exact 
-						path="/explore" 
-						render={() => <h1>Explore</h1>} />
+							<Route 
+								exact 
+								path="/my-profile" 
+								render={() => <h1>My Profile</h1>} />
 
-					{/* FUTURE DEVELOPMENT: Explore Dropdown */}
-					{/* <Route 
-						exact 
-						path="/by-region" 
-						render={() => <h1>Region</h1>} />
+							<Route 
+								exact 
+								path="/explore" 
+								render={() => <h1>Explore</h1>} />
 
-					<Route 
-						exact 
-						path="/by-country" 
-						render={() => <h1>Country</h1>} />
-					<Route 
-						exact 
-						path="/by-city" 
-						render={() => <h1>City</h1>} />
-					<Route 
-						exact 
-						path="/by-target-audience" 
-						render={() => <h1>Target Audience</h1>} />
-					<Route 
-						exact 
-						path="/by-duration" 
-						render={() => <h1>Duration</h1>} /> */}
+							{/* FUTURE DEVELOPMENT: Explore Dropdown */}
+							{/* <Route 
+								exact 
+								path="/by-region" 
+								render={() => <h1>Region</h1>} />
 
-					<Route 
-						exact 
-						path="/log-out" 
-						render={() => <h1>Log Out</h1>} />
-					
-					<Route 
-						exact 
-						path="/about-us" 
-						render={() => <h1>About Us</h1>} />
-					
-					<Route 
-						exact 
-						path="/community-guidelines" 
-						render={() => <h1>Community Guidelines</h1>} />
+							<Route 
+								exact 
+								path="/by-country" 
+								render={() => <h1>Country</h1>} />
+							<Route 
+								exact 
+								path="/by-city" 
+								render={() => <h1>City</h1>} />
+							<Route 
+								exact 
+								path="/by-target-audience" 
+								render={() => <h1>Target Audience</h1>} />
+							<Route 
+								exact 
+								path="/by-duration" 
+								render={() => <h1>Duration</h1>} /> */}
 
-					<Route 
-						exact 
-						path="/sign-in" render={() => < SignInForm />} />
+							<Route 
+								exact 
+								path="/log-out" 
+								render={() => <h1>Log Out</h1>} />
+							
+							<Route 
+								exact 
+								path="/about-us" 
+								render={() => <h1>About Us</h1>} />
+							
+							<Route 
+								exact 
+								path="/community-guidelines" 
+								render={() => <h1>Community Guidelines</h1>} />
 
-					<Route 
-						exact 
-						path="/sign-up" 
-						render={() => <SignUpForm />} />
-				</Switch>
-			</Container>
-		</div>
+							<Route 
+								exact 
+								path="/sign-in" render={() => < SignInForm />} />
+
+							<Route 
+								exact 
+								path="/sign-up" 
+								render={() => <SignUpForm />} />
+						</Switch>
+					</Container>
+				</div>
+			</SetCurrentUserContext.Provider>
+		</CurrentUserContext.Provider>
 	);
 }
 
