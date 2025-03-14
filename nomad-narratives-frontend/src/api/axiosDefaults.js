33f6929ad4a6ed06ef9e9816cc28120f0cffc00b
Maeve Crossan <maevecrossan@gmail.com>
2025-03-14 16:58:27 +0000

    Remove baseURL.

--- nomad-narratives-frontend/src/api/axiosDefaults.js
+++ nomad-narratives-frontend/src/api/axiosDefaults.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://nomad-narratives-05968209a16d.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
 
