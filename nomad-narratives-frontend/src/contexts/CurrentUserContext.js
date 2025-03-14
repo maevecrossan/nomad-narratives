5071b36befbc9c3ab657234508c4efb15339a9e3
Maeve Crossan <maevecrossan@gmail.com>
2025-03-14 12:53:59 +0000

    Use response interceptors and useMemo to extend access and token duration.

--- nomad-narratives-frontend/src/contexts/CurrentUserContext.js
+++ nomad-narratives-frontend/src/contexts/CurrentUserContext.js
@@ -3,21 +3,23 @@
  * and making API call when component mounts.)
 */
 
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useMemo, useState, useHistory } from 'react';
 import axios from 'axios';
+import { axiosRes } from '../api/axiosDefaults';
 
-export const CurrentUserContext = createContext()
-export const SetCurrentUserContext = createContext()
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
 
-export const useCurrentUser = () => useContext(CurrentUserContext)
-export const useSetCurrentUser = () => useContext(SetCurrentUserContext)
+export const useCurrentUser = () => useContext(CurrentUserContext);
+export const useSetCurrentUser = () => useContext(SetCurrentUserContext);
 
 export const CurrentUserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null)
+    const [currentUser, setCurrentUser] = useState(null);
+	const history = useHistory()
 
 	const handleMount = async () => {
 		try {
-			const {data} = await axios.get('dj-rest-auth/user/')
+			const {data} = await axiosRes.get('dj-rest-auth/user/')
 			setCurrentUser(data)
 		} catch(err) {
 			console.log(err)
@@ -27,6 +29,28 @@ export const CurrentUserProvider = ({ children }) => {
 	useEffect(() => {
 		handleMount()
 	}, [])
+
+	useMemo(() => {
+		axiosRes.interceptors.response.use(
+			(response) => response,
+			async (err) => {
+				if (err.response?.status === 401) {
+					try {
+						await axios.post('dj-rest-auth/token/refresh/')
+					} catch (err) {
+						setCurrentUser((prevCurrentUser) => {
+							if (prevCurrentUser) {
+								history.push('/sign-in');
+							}
+							return null;
+						})
+					}
+					return axios(err.config);
+				} 
+				return Promise.reject(err)
+			}
+		)
+	});
 
     return(
         <CurrentUserContext.Provider value={currentUser}>
