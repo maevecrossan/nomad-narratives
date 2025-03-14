1e57382e7381acef604e13fdfc382c049bc103d5
Maeve Crossan <maevecrossan@gmail.com>
2025-03-14 08:26:44 +0000

    Remove unused styles reference.

--- nomad-narratives-frontend/src/pages/auth/SignInForm.js
+++ nomad-narratives-frontend/src/pages/auth/SignInForm.js
@@ -36,7 +36,7 @@ const SignInForm = () => {
 
 	return (
         <Container
-		className={`${styles.SignUpForm} d-flex flex-column align-items-center justify-content-center ${styles["si-bg-image"]}`}
+		className={`d-flex flex-column align-items-center justify-content-center ${styles["si-bg-image"]}`}
 		>
 		<Row>
 			<Col>
