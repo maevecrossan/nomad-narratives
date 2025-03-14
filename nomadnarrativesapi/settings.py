a4f23a941642302d75e7435d0b593f7d6069df14
Maeve Crossan <maevecrossan@gmail.com>
2025-03-14 17:11:50 +0000

    Update static root and add whitenoise root.

--- nomadnarrativesapi/settings.py
+++ nomadnarrativesapi/settings.py
@@ -213,7 +213,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+WHITENOISE_ROOT = BASE_DIR / 'staticfiles' / 'build'
 STATICFILES_STORAGE = (
     'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 )
