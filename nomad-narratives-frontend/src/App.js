61f8443428b2e562813dba10fea78ce22457cca1
Maeve Crossan <maevecrossan@gmail.com>
2025-03-14 08:59:33 +0000

    Add handleMount and useEffect hook to check who the current user is.

--- nomad-narratives-frontend/src/App.js
+++ nomad-narratives-frontend/src/App.js
@@ -7,83 +7,99 @@ import './api/axiosDefaults';
 import SignUpForm from './pages/auth/SignUpForm';
 import WelcomePage from './pages/WelcomePage';
 import SignInForm from './pages/auth/SignInForm';
+import { useEffect, useState } from 'react';
 
 function App() {
-return (
-	<div className={styles.App}>
-		< NavBar />
-		<Container className={styles.Main}>
-			<Switch>
-				<Route 
-					exact 
-					path="/welcome" 
-					render={() => <WelcomePage/>} />
+	const [currentUser, setCurrentUser] = useState(null)
 
-				<Route 
-					exact 
-					path="/my-feed" 
-					render={() => <h1>My Feed</h1>} />
+	const handleMount = async () => {
+		try {
+			const {data} = await axios.get('dj-rest-auth/user/')
+			setCurrentUser(data)
+		} catch(err) {
+			console.log(err)
+		}
+	}
 
-				<Route 
-					exact 
-					path="/my-profile" 
-					render={() => <h1>My Profile</h1>} />
+	useEffect(() => {
+		handleMount()
+	}, [])
 
-				<Route 
-					exact 
-					path="/explore" 
-					render={() => <h1>Explore</h1>} />
+	return (
+		<div className={styles.App}>
+			< NavBar />
+			<Container className={styles.Main}>
+				<Switch>
+					<Route 
+						exact 
+						path="/welcome" 
+						render={() => <WelcomePage/>} />
 
-				{/* FUTURE DEVELOPMENT: Explore Dropdown */}
-				{/* <Route 
-					exact 
-					path="/by-region" 
-					render={() => <h1>Region</h1>} />
+					<Route 
+						exact 
+						path="/my-feed" 
+						render={() => <h1>My Feed</h1>} />
 
-				<Route 
-					exact 
-					path="/by-country" 
-					render={() => <h1>Country</h1>} />
-				<Route 
-					exact 
-					path="/by-city" 
-					render={() => <h1>City</h1>} />
-				<Route 
-					exact 
-					path="/by-target-audience" 
-					render={() => <h1>Target Audience</h1>} />
-				<Route 
-					exact 
-					path="/by-duration" 
-					render={() => <h1>Duration</h1>} /> */}
+					<Route 
+						exact 
+						path="/my-profile" 
+						render={() => <h1>My Profile</h1>} />
 
-				<Route 
-					exact 
-					path="/log-out" 
-					render={() => <h1>Log Out</h1>} />
-				
-				<Route 
-					exact 
-					path="/about-us" 
-					render={() => <h1>About Us</h1>} />
-				
-				<Route 
-					exact 
-					path="/community-guidelines" 
-					render={() => <h1>Community Guidelines</h1>} />
+					<Route 
+						exact 
+						path="/explore" 
+						render={() => <h1>Explore</h1>} />
 
-				<Route 
-					exact 
-					path="/sign-in" render={() => < SignInForm />} />
+					{/* FUTURE DEVELOPMENT: Explore Dropdown */}
+					{/* <Route 
+						exact 
+						path="/by-region" 
+						render={() => <h1>Region</h1>} />
 
-				<Route 
-					exact 
-					path="/sign-up" 
-					render={() => <SignUpForm />} />
-			</Switch>
-		</Container>
-	</div>
-);
+					<Route 
+						exact 
+						path="/by-country" 
+						render={() => <h1>Country</h1>} />
+					<Route 
+						exact 
+						path="/by-city" 
+						render={() => <h1>City</h1>} />
+					<Route 
+						exact 
+						path="/by-target-audience" 
+						render={() => <h1>Target Audience</h1>} />
+					<Route 
+						exact 
+						path="/by-duration" 
+						render={() => <h1>Duration</h1>} /> */}
+
+					<Route 
+						exact 
+						path="/log-out" 
+						render={() => <h1>Log Out</h1>} />
+					
+					<Route 
+						exact 
+						path="/about-us" 
+						render={() => <h1>About Us</h1>} />
+					
+					<Route 
+						exact 
+						path="/community-guidelines" 
+						render={() => <h1>Community Guidelines</h1>} />
+
+					<Route 
+						exact 
+						path="/sign-in" render={() => < SignInForm />} />
+
+					<Route 
+						exact 
+						path="/sign-up" 
+						render={() => <SignUpForm />} />
+				</Switch>
+			</Container>
+		</div>
+	);
 }
 
 export default App;
