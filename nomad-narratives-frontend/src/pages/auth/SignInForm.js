e90b23589d6778bd0bb8810905d3772ba0bff3e3
Maeve Crossan <maevecrossan@gmail.com>
2025-03-16 16:05:00 +0000

    Use 'prettier' to format.

--- nomad-narratives-frontend/src/pages/auth/SignInForm.js
+++ nomad-narratives-frontend/src/pages/auth/SignInForm.js
@@ -1,116 +1,117 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { Container, Form, Button, Row, Col, Alert } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom/cjs/react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
-import axios from 'axios';
+import axios from "axios";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 
 const SignInForm = () => {
-	const setCurrentUser = useSetCurrentUser();
+    const setCurrentUser = useSetCurrentUser();
 
-	const [signInData, setSignInData] = useState({
-            username: '',
-            password: '',
-        });
-    
-    const { username, password} = signInData;
+    const [signInData, setSignInData] = useState({
+        username: "",
+        password: "",
+    });
+
+    const { username, password } = signInData;
 
     const [errors, setErrors] = useState({});
-    
+
     const history = useHistory();
 
     const handleChange = (event) => {
-		setSignInData({
-			...signInData,
-			[event.target.name]: event.target.value,
-		})
-	}
+        setSignInData({
+            ...signInData,
+            [event.target.name]: event.target.value,
+        });
+    };
 
     const handleSubmit = async (event) => {
-		event.preventDefault();
-		try {
-			const {data} = await axios.post("/dj-rest-auth/login/", signInData);
-			setCurrentUser(data.user)
-			history.push("/my-feed");
-		} catch (err) {
-			setErrors(err.response?.data);
-		}
-	};
+        event.preventDefault();
+        try {
+            const { data } = await axios.post(
+                "/dj-rest-auth/login/",
+                signInData
+            );
+            setCurrentUser(data.user);
+            history.push("/my-feed");
+        } catch (err) {
+            setErrors(err.response?.data);
+        }
+    };
 
-	return (
+    return (
         <Container
-		className={`d-flex flex-column align-items-center justify-content-center ${styles["si-bg-image"]}`}
-		>
-		<Row>
-			<Col>
-				<h1 className={`${styles.Header}`}>Sign In</h1>
-			</Col>
-		</Row>
+            className={`d-flex flex-column align-items-center justify-content-center ${styles["si-bg-image"]}`}
+        >
+            <Row>
+                <Col>
+                    <h1 className={`${styles.Header}`}>Sign In</h1>
+                </Col>
+            </Row>
 
-		<Row>
-			<Col>
-				<Form onSubmit={handleSubmit}>
-					<Form.Group controlId="username">
-					<Form.Label className="d-none">username</Form.Label>
-					<Form.Control
-						className={styles.Input}
-						type="text"
-						placeholder="Username"
-						name="username"
-						value={username}
-						onChange={handleChange}
-					/>
-					</Form.Group>
-					{errors.username?.map((message, idx) => (
-						<Alert variant="warning" key={idx}>
-							{message}
-						</Alert>
-					))}
+            <Row>
+                <Col>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group controlId="username">
+                            <Form.Label className="d-none">username</Form.Label>
+                            <Form.Control
+                                className={styles.Input}
+                                type="text"
+                                placeholder="Username"
+                                name="username"
+                                value={username}
+                                onChange={handleChange}
+                            />
+                        </Form.Group>
+                        {errors.username?.map((message, idx) => (
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
 
-					<Form.Group controlId="password">
-					<Form.Label className="d-none">Password</Form.Label>
-					<Form.Control
-						className={styles.Input}
-						type="password"
-						placeholder="Password"
-						name="password"
-						value={password}
-						onChange={handleChange}
-					/>
-					</Form.Group>
-					{errors.password?.map((message, idx) => (
-						<Alert variant="warning" key={idx}>
-							{message}
-						</Alert>
-					))}
+                        <Form.Group controlId="password">
+                            <Form.Label className="d-none">Password</Form.Label>
+                            <Form.Control
+                                className={styles.Input}
+                                type="password"
+                                placeholder="Password"
+                                name="password"
+                                value={password}
+                                onChange={handleChange}
+                            />
+                        </Form.Group>
+                        {errors.password?.map((message, idx) => (
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
 
-					<Button
-					className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
-					type="submit"
-					>
-						Sign in
-					</Button>
-					{errors.non_field_errors?.map((message, idx) => (
-						<Alert key={idx} variant="warning" className="mt-3">
-							{message}
-						</Alert>
-					))}
+                        <Button
+                            className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+                            type="submit"
+                        >
+                            Sign in
+                        </Button>
+                        {errors.non_field_errors?.map((message, idx) => (
+                            <Alert key={idx} variant="warning" className="mt-3">
+                                {message}
+                            </Alert>
+                        ))}
+                    </Form>
+                </Col>
+            </Row>
 
-				</Form>
-			</Col>
-		</Row>
-
-		<Row>
-			<Col>
-				<Link className={styles.Link} to="/sign-up">
-					Don't have an account? Sign up here!
-				</Link>
-			</Col>
-		</Row>
-
-		</Container>
-	);
+            <Row>
+                <Col>
+                    <Link className={styles.Link} to="/sign-up">
+                        Don't have an account? Sign up here!
+                    </Link>
+                </Col>
+            </Row>
+        </Container>
+    );
 };
 
 export default SignInForm;
