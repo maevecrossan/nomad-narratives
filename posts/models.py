285326a9352ac7164d35415a44ae34f0a3e6f443
Maeve Crossan <maevecrossan@gmail.com>
2025-03-16 18:37:57 +0000

    Remove image_filter field.

--- posts/models.py
+++ posts/models.py
@@ -24,23 +24,6 @@ class TripPost(models.Model):
     This model does not handle the details of the trip (see TripDetails model).
     '''
 
-    image_filter_choices = [
-        ('_1977', '1977'),
-        ('brannan', 'Brannan'),
-        ('earlybird', 'Earlybird'),
-        ('hudson', 'Hudson'),
-        ('inkwell', 'Inkwell'),
-        ('lofi', 'Lo-Fi'),
-        ('kelvin', 'Kelvin'),
-        ('normal', 'Normal'),
-        ('nashville', 'Nashville'),
-        ('rise', 'Rise'),
-        ('toaster', 'Toaster'),
-        ('valencia', 'Valencia'),
-        ('walden', 'Walden'),
-        ('xpro2', 'X-pro II')
-    ]
-
     owner = models.ForeignKey(
         User,
         on_delete=models.CASCADE
@@ -68,11 +51,6 @@ class TripPost(models.Model):
         blank=False,
         help_text="Please describe your image in case\
               there is an issue displaying it."
-        )
-    image_filter = models.CharField(
-        max_length=32,
-        choices=image_filter_choices,
-        default='normal'
         )
 
     class Meta:
--- posts/serializers.py
+++ posts/serializers.py
@@ -144,6 +144,6 @@ class TripPostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'image_filter', 'image_alt_text',
+            'title', 'content', 'image', 'image_alt_text',
             'like_id', 'likes_count', 'comments_count', 'details'
         ]
