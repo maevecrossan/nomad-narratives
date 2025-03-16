f6be71a176b4fe904e12faf21df18ec735cee694
Maeve Crossan <maevecrossan@gmail.com>
2025-03-16 19:18:52 +0000

    Import useEffect.

--- nomad-narratives-frontend/src/pages/posts/TripPostCreateForm.js
+++ nomad-narratives-frontend/src/pages/posts/TripPostCreateForm.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
