8471a05661c8327f6ffc7ac25035b8ffbfab93ae
Maeve Crossan <maevecrossan@gmail.com>
2025-03-16 13:45:09 +0000

    Update debug settings and replace secret key.

--- nomadnarrativesapi/settings.py
+++ nomadnarrativesapi/settings.py
@@ -36,7 +36,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') == '1'
+if os.environ.get('ENV') == 'production':
+    DEBUG = False  # Disable debugging in production
+else:
+    DEBUG = os.environ.get('DEBUG') == '1'  # Enable debugging if DEBUG = '1'
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
