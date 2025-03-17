768efd929e4fa24ef2b1847917f4724c7d8af9d7
Maeve Crossan <maevecrossan@gmail.com>
2025-03-17 22:07:17 +0000

    Uncomment after debugging attempt.

--- posts/models.py
+++ posts/models.py
@@ -1,25 +1,15 @@
-'''
-This file houses two models.
-
-1. TripPost:
-This model contains the logic associated with the post itself.
-
-2. TripDetails:
-This model contains the logic associated with the finer details
-of the trip (location, duration, etc).
-'''
 
 from django.db import models
 from django.contrib.auth.models import User
-# from cities_light.models import City, Country
-# from utils.continents import get_continent_by_country
+from cities_light.models import City, Country
+from utils.continents import get_continent_by_country
 
 
 class TripPost(models.Model):
     '''
     Trip Post model, related to 'owner', i.e. a User instance.
     Default image set so that we can always reference image.url.
-    
+
     This model handles the content of the post (image, article content, title).
     This model does not handle the details of the trip (see TripDetails model).
     '''
@@ -55,7 +45,7 @@ class TripPost(models.Model):
 
     class Meta:
         '''
-        Specifies ordering of posts.
+        Specififes ordering of posts.
         '''
         ordering = ['-created_at']
 
@@ -66,7 +56,7 @@ class TripPost(models.Model):
 class TripDetails(models.Model):
     '''
     Outlines the additional details a user will add to their TripPost.
-    
+
     These details help categorize and filter trip content based on location,
     traveler demographics, and trip length, providing a richer context
     for the main TripPost
@@ -75,45 +65,45 @@ class TripDetails(models.Model):
         TripPost, on_delete=models.CASCADE, related_name="details"
         )
 
-    # continent = models.CharField(max_length=20, blank=True, editable=False)
+    continent = models.CharField(max_length=20, blank=True, editable=False)
 
-    # country = models.ForeignKey(Country, on_delete=models.PROTECT)
+    country = models.ForeignKey(Country, on_delete=models.PROTECT)
 
-    # city = models.ManyToManyField(City)
+    city = models.ManyToManyField(City)
 
-    # traveller_number = models.PositiveIntegerField()
+    traveller_number = models.PositiveIntegerField()
 
-    # relevant_for_choices = [
-    #         ('all', 'All Genders & Orientations'),
-    #         ('women', 'Women'),
-    #         ('men', 'Men'),
-    #         ('nonbinary', 'Non-Binary'),
-    #         ('lgbtq', 'LGBTQ+ Travelers'),
-    #     ]
-    # relevant_for = models.CharField(
-    #     max_length=10, choices=relevant_for_choices
-    #     )
+    relevant_for_choices = [
+            ('all', 'All Genders & Orientations'),
+            ('women', 'Women'),
+            ('men', 'Men'),
+            ('nonbinary', 'Non-Binary'),
+            ('lgbtq', 'LGBTQ+ Travelers'),
+        ]
+    relevant_for = models.CharField(
+        max_length=10, choices=relevant_for_choices
+        )
 
-    # duration_value = models.PositiveBigIntegerField()
+    duration_value = models.PositiveBigIntegerField()
 
-    # duration_unit_choices = [
-    #     ('days', 'Day(s)'),
-    #     ('weeks', 'Week(s)'),
-    #     ('months', 'Month(s)'),
-    #     ('years', 'Year(s)'),
-    # ]
+    duration_unit_choices = [
+        ('days', 'Day(s)'),
+        ('weeks', 'Week(s)'),
+        ('months', 'Month(s)'),
+        ('years', 'Year(s)'),
+    ]
 
-    # duration_unit = models.CharField(
-    #     max_length=10, choices=duration_unit_choices
-    #     )
+    duration_unit = models.CharField(
+        max_length=10, choices=duration_unit_choices
+        )
 
-    # def save(self, *args, **kwargs):
-    #     """
-    #     Automatically set the continent based on the selected country.
-    #     """
-    #     if self.country:
-    #         self.continent = get_continent_by_country(self.country.name)
-    #     super().save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        """
+        Automatically set the continent based on the selected country.
+        """
+        if self.country:
+            self.continent = get_continent_by_country(self.country.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return (
