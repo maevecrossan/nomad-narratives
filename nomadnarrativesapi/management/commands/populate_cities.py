a7cb02c45c874b6e215519626f0937bacd95c79d
Maeve Crossan <maevecrossan@gmail.com>
2025-03-17 22:12:26 +0000

    Uncomment after debugging attempt.

--- nomadnarrativesapi/management/commands/populate_cities.py
+++ nomadnarrativesapi/management/commands/populate_cities.py
@@ -7,22 +7,22 @@ It retrieves the list of all countries and for each country, calls the
 to fetch and add city data for that country.
 '''
 
-# from django.core.management.base import BaseCommand
-# from utils.continents import populate_cities_for_all_countries
+from django.core.management.base import BaseCommand
+from utils.continents import populate_cities_for_all_countries
 
 
-# class Command(BaseCommand):
-#     '''
-#     Django management command to populate the database with cities for
-# all countries.
+class Command(BaseCommand):
+    '''
+    Django management command to populate the database with cities for
+    all countries.
 
-#     This command interacts with the GeoNames API to fetch and add cities
-# to the
-#     `City` model for every country stored in the `Country` model.
-#     '''
-#     help = 'Populates cities for all countries in the database'
+    This command interacts with the GeoNames API to fetch and add cities
+    to the
+    `City` model for every country stored in the `Country` model.
+    '''
+    help = 'Populates cities for all countries in the database'
 
-#     def handle(self, *args, **kwargs):
-#         self.stdout.write("Starting to populate cities for all countries...")
-#         populate_cities_for_all_countries()
-#         self.stdout.write("Finished populating cities.")
+    def handle(self, *args, **kwargs):
+        self.stdout.write("Starting to populate cities for all countries...")
+        populate_cities_for_all_countries()
+        self.stdout.write("Finished populating cities.")
