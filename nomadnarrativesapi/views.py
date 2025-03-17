9d8cba12c6cc0b5788667993fe98102e2138becd
Maeve Crossan <maevecrossan@gmail.com>
2025-03-17 09:17:54 +0000

    Refactor views for country and city.

--- nomadnarrativesapi/views.py
+++ nomadnarrativesapi/views.py
@@ -4,15 +4,16 @@ Root Route View
 This module defines the root route for the Django REST framework API.
 It provides a simple welcome message when accessed.
 '''
-from django.shortcuts import get_object_or_404
+# from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .settings import (
     JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
     JWT_AUTH_SECURE,
 )
+from rest_framework import viewsets
 from cities_light.models import Country, City
-# from utils.continents import fetch_and_add_cities_for_country
+from .serializers import CountrySerializer, CitySerializer
 
 
 @api_view()
@@ -45,27 +46,21 @@ def logout_route(request):
     return response
 
 
-@api_view(['GET'])
-def get_all_countries(request):
-    '''
-    Returns a list of all countries.
-    '''
-    countries = Country.objects.all()
-    country_list = [
-        {"id": country.id, "name": country.name} for country in countries
-        ]
-    return Response({"countries": country_list})
+class CountryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
 
 
-@api_view(['GET'])
-def cities_by_country(request, country_id):
-    '''
-    Returns a list of cities for the given country.
-    If no cities are found, it fetches and adds them to the database.
-    '''
-    country = get_object_or_404(Country, id=country_id)
-    cities = City.objects.filter(country=country)
+class CityViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
 
-    city_list = [{"id": city.id, "name": city.name} for city in cities]
-
-    return Response({"cities": city_list})
+    def get_queryset(self):
+        ''''
+        Filter cities by country if a query parameter is provided.
+        '''
+        queryset = City.objects.all()
+        country_id = self.request.query_params.get('country_id')
+        if country_id:
+            queryset = queryset.filter(country_id=country_id)
+        return queryset
