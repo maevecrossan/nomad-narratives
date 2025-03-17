521bdb0b28580b49f7b426d433c96a2a5b73afbe
Maeve Crossan <maevecrossan@gmail.com>
2025-03-17 09:20:17 +0000

    Add serializer for city and country.

--- nomadnarrativesapi/serializers.py
+++ nomadnarrativesapi/serializers.py
@@ -1,10 +1,13 @@
 '''
-This module defines a serializer for the current user details, including the user's profile information.
-It extends the `UserDetailsSerializer` from the `dj_rest_auth` package, adding fields for the user's profile ID 
+This module defines a serializer for the current user details,
+including the user's profile information.
+It extends the `UserDetailsSerializer` from the `dj_rest_auth`
+package, adding fields for the user's profile ID
 and profile image URL.
 '''
 from dj_rest_auth.serializers import UserDetailsSerializer
 from rest_framework import serializers
+from cities_light.models import Country, City
 
 
 class CurrentUserSerializer(UserDetailsSerializer):
@@ -22,3 +25,15 @@ class CurrentUserSerializer(UserDetailsSerializer):
         fields = UserDetailsSerializer.Meta.fields + (
             'profile_id', 'profile_image'
             )
+
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = ['id', 'name']
+
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = ['id', 'name', 'country']
