e1a311ecb0c031d826bd902f891b0683769b05a6
Maeve Crossan <maevecrossan@gmail.com>
2025-03-18 08:56:39 +0000

    Remove old views and create new views from cities_light models.

--- nomadnarrativesapi/views.py
+++ nomadnarrativesapi/views.py
@@ -11,16 +11,13 @@ from .settings import (
     JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
     JWT_AUTH_SECURE,
 )
-from rest_framework import viewsets, permissions, status
-from rest_framework.pagination import PageNumberPagination
 from cities_light.models import Country, City
-from .serializers import CountrySerializer, CitySerializer
-from rest_framework.decorators import action
+from rest_framework import status
 
 
-@api_view()
-def root_route(request):
-    return Response({"message": "Welcome to my django rest framework API!"})
+# @api_view()
+# def root_route(request):
+#     return Response({"message": "Welcome to my django rest framework API!"})
 
 
 # dj-rest-auth logout view fix
@@ -48,31 +45,59 @@ def logout_route(request):
     return response
 
 
-class CountryPagination(PageNumberPagination):
-    page_size = 500
+# Fetch all countries
+@api_view(['GET'])
+def get_countries(request):
+    """
+    This view returns a list of all countries.
+    """
+    countries = Country.objects.all()
+    data = [{'id': country.id, 'name': country.name} for country in countries]
+    return Response(data)
 
 
-class CountryViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Country.objects.all()
-    serializer_class = CountrySerializer
-    pagination_class = CountryPagination
+# Fetch cities for a selected country
+@api_view(['GET'])
+def get_cities(request, country_id):
+    """
+    This view returns cities for a selected country.
+    """
+    try:
+        country = Country.objects.get(id=country_id)
+    except Country.DoesNotExist:
+        return Response(
+            {"error": "Country not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+    cities = City.objects.filter(country=country)
+    data = [{'id': city.id, 'name': city.name} for city in cities]
+    return Response(data)
+
+# class CountryPagination(PageNumberPagination):
+#     page_size = 500
 
 
-class CityViewSet(viewsets.ModelViewSet):
-    queryset = City.objects.all()
-    serializer_class = CitySerializer
-    permission_classes = [permissions.IsAuthenticated]
+# class CountryViewSet(viewsets.ReadOnlyModelViewSet):
+#     queryset = Country.objects.all()
+#     serializer_class = CountrySerializer
+#     pagination_class = CountryPagination
 
-    # This custom action allows filtering cities by country
-    @action(detail=False, methods=['get'], url_path='by-country')
-    def get_cities_by_country(self, request):
-        country_id = request.query_params.get('country', None)
-        if country_id:
-            cities = City.objects.filter(country_id=country_id)
-            serializer = self.get_serializer(cities, many=True)
-            return Response(serializer.data)
-        else:
-            return Response(
-                {"detail": "Country ID is required."},
-                status=status.HTTP_400_BAD_REQUEST
-                )
+
+# class CityViewSet(viewsets.ModelViewSet):
+#     queryset = City.objects.all()
+#     serializer_class = CitySerializer
+#     permission_classes = [permissions.IsAuthenticated]
+
+#     # This custom action allows filtering cities by country
+#     @action(detail=False, methods=['get'], url_path='by-country')
+#     def get_cities_by_country(self, request):
+#         country_id = request.query_params.get('country', None)
+#         if country_id:
+#             cities = City.objects.filter(country_id=country_id)
+#             serializer = self.get_serializer(cities, many=True)
+#             return Response(serializer.data)
+#         else:
+#             return Response(
+#                 {"detail": "Country ID is required."},
+#                 status=status.HTTP_400_BAD_REQUEST
+#                 )
