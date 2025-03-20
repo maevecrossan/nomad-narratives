032fd01f3e761fba46144637cc73f1bd7266e186
Maeve Crossan <maevecrossan@gmail.com>
2025-03-20 16:45:37 +0000

    Change tripPost/setTripPost to 'tripPost.results[0]' rather than 'null'.

--- nomad-narratives-frontend/src/pages/posts/TripPostPage.js
+++ nomad-narratives-frontend/src/pages/posts/TripPostPage.js
@@ -7,10 +7,11 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
+import TripPost from "./TripPost";
 
 function TripPostPage() {
     const { id } = useParams();
-    const [tripPost, setTripPost] = useState(null);
+    const [tripPost, setTripPost] = useState({ results: [] });
 
     useEffect(() => {
         const handleMount = async () => {
@@ -31,7 +32,7 @@ function TripPostPage() {
         <Row className="h-100">
             <Col className="py-2 p-0 p-lg-2" lg={8}>
                 <p>Popular profiles for mobile</p>
-                <p>Post component</p>
+                < TripPost {...tripPost.results[0]} setTripPost={setTripPost}/>
                 <Container className={appStyles.Content}>Comments</Container>
             </Col>
             <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
