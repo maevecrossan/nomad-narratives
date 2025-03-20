d663fe17140c8bf8b2a224aae243de6e26220711
Maeve Crossan <maevecrossan@gmail.com>
2025-03-20 16:43:28 +0000

    Create file and add placeholder text.

---
+++ nomad-narratives-frontend/src/pages/posts/TripPost.js
@@ -0,0 +1,10 @@
+import React from 'react'
+// import styles from "../../styles/TripPost.module.css"
+
+const TripPost = (props) => {
+  return (
+    <div>TripPost Placeholder text</div>
+  )
+}
+
+export default TripPost
