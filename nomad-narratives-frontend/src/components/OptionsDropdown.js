8aac346a9b63966f357877394cdcc2a2029ce866
Maeve Crossan <maevecrossan@gmail.com>
2025-03-22 17:11:40 +0000

    Add poperConfig to make sure the dropdown menu is positioned consistently across browsers.

--- nomad-narratives-frontend/src/components/OptionsDropdown.js
+++ nomad-narratives-frontend/src/components/OptionsDropdown.js
@@ -18,12 +18,12 @@ const ThreeDots = React.forwardRef(({ onClick }, ref) => (
 export const OptionsDropdown = () => {
     return (
         <Dropdown className="ml-auto" drop="left">
-        <Dropdown.Toggle as={ThreeDots} />
+            <Dropdown.Toggle as={ThreeDots} />
 
-        <Dropdown.Menu className="text-center">
-            <Dropdown.Item eventKey="1">Red</Dropdown.Item>
-            <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
-        </Dropdown.Menu>
-    </Dropdown>
-    )
-}
+            <Dropdown.Menu className="text-center" popperConfig={{ strategy: "fixed" }}>
+                <Dropdown.Item>Edit</Dropdown.Item>
+                <Dropdown.Item>Delete</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+    );
+};
