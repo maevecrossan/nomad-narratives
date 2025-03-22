e015899728b05218dbe746cffe6e4fab9bb25c18
Maeve Crossan <maevecrossan@gmail.com>
2025-03-22 18:51:56 +0000

    Add comment form input to post page.

--- nomad-narratives-frontend/src/pages/posts/TripPostPage.js
+++ nomad-narratives-frontend/src/pages/posts/TripPostPage.js
@@ -8,22 +8,27 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
 import TripPost from "./TripPost";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function TripPostPage() {
     const { id } = useParams();
     const [tripPost, setTripPost] = useState({ results: [] });
+    const currentUser = useCurrentUser();
+    const profile_image = currentUser?.profile_image;
+    const [comments, setComments] = useState({ results: [] });
 
     useEffect(() => {
         const handleMount = async () => {
             try {
                 const [{ data: tripPost }] = await Promise.all([
                     axiosReq.get(`/posts/${id}`),
-                ])
+                ]);
                 setTripPost({ results: [tripPost] });
-            } catch(err) {
+            } catch (err) {
                 console.log(err);
             }
-        }
+        };
         handleMount();
     }, [id]);
 
@@ -31,8 +36,25 @@ function TripPostPage() {
         <Row className="h-100">
             <Col className="py-2 p-0 p-lg-2" lg={8}>
                 <p>Popular profiles for mobile</p>
-                < TripPost {...tripPost.results[0]} setTripPost={setTripPost} TripPostPage />
-                <Container className={appStyles.Content}>Comments</Container>
+                <TripPost
+                    {...tripPost.results[0]}
+                    setTripPost={setTripPost}
+                    TripPostPage
+                />
+                <Container className={appStyles.Content}>
+                    Comments
+                    {currentUser ? (
+                        <CommentCreateForm
+                            profile_id={currentUser.profile_id}
+                            profileImage={profile_image}
+                            tripPost={id}
+                            setTripPost={setTripPost}
+                            setComments={setComments}
+                        />
+                    ) : comments.results.length ? (
+                        "Comments"
+                    ) : null}
+                </Container>
             </Col>
             <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
                 Popular profiles for desktop
