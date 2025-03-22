7de0f8ee32adbefadd7153c849f9c8deaccbd3a4
Maeve Crossan <maevecrossan@gmail.com>
2025-03-22 17:16:08 +0000

    Add edit and delete to respective dropdown items.

--- nomad-narratives-frontend/src/components/OptionsDropdown.js
+++ nomad-narratives-frontend/src/components/OptionsDropdown.js
@@ -15,14 +15,29 @@ const ThreeDots = React.forwardRef(({ onClick }, ref) => (
     />
 ));
 
-export const OptionsDropdown = () => {
+export const OptionsDropdown = ({ handleEdit, handleDelete }) => {
     return (
         <Dropdown className="ml-auto" drop="left">
             <Dropdown.Toggle as={ThreeDots} />
 
-            <Dropdown.Menu className="text-center" popperConfig={{ strategy: "fixed" }}>
-                <Dropdown.Item>Edit</Dropdown.Item>
-                <Dropdown.Item>Delete</Dropdown.Item>
+            <Dropdown.Menu
+                className="text-center"
+                popperConfig={{ strategy: "fixed" }}
+            >
+                <Dropdown.Item
+                    className={styles.DropdownItem}
+                    onClick={handleEdit}
+                    aria-label="edit"
+                >
+                    <i className="fas fa-edit" />
+                </Dropdown.Item>
+                <Dropdown.Item
+                    className={styles.DropdownItem}
+                    onClick={handleDelete}
+                    aria-label="delete"
+                >
+                    <i className="fas fa-trash-alt" />
+                </Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
     );
