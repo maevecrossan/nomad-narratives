eeca7a4474ee7ac647dea9e9ba85856316bdc6c7
Maeve Crossan <maevecrossan@gmail.com>
2025-03-23 14:09:16 +0000

    Enabled popular profiles to be fetched on mount through useEffect hook.

--- nomad-narratives-frontend/src/pages/profiles/PopularProfiles.js
+++ nomad-narratives-frontend/src/pages/profiles/PopularProfiles.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import { axiosReq } from '../../api/axiosDefaults';
 import appStyles from '../../App.module.css';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
 
 const PopularProfiles = () => {
     const [profileData, setProfileData] = useState({
@@ -9,6 +11,25 @@ const PopularProfiles = () => {
         popularProfiles: { results: [] },
     });
     const { popularProfiles } = profileData;
+    const currentUser = useCurrentUser();
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const {data} = await axiosReq.get(
+                    'profiles/?ordering=-followers_count'
+                );
+                setProfileData(prevState => ({
+                    ...prevState,
+                    popularProfiles: data,
+                }))
+            } catch(err) {
+                console.log(err);
+            }
+        };
+        handleMount();
+    }, [currentUser]);
+
     return (
         <Container className={appStyles.Content}>
             <p>Most followed profiles:</p>
