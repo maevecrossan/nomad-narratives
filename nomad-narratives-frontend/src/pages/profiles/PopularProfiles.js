8ca4e7d381aef46f3e834c742ee83b24f091ea9c
Maeve Crossan <maevecrossan@gmail.com>
2025-03-23 15:04:44 +0000

    Remove profile data into ProfileDataContext.js so it can be accessed and synced across the app.

--- nomad-narratives-frontend/src/pages/profiles/PopularProfiles.js
+++ nomad-narratives-frontend/src/pages/profiles/PopularProfiles.js
@@ -7,30 +7,6 @@ import Asset from "../../components/Asset";
 import Profile from "./Profile";
 
 const PopularProfiles = ({ mobile }) => {
-    const [profileData, setProfileData] = useState({
-        // Add profile page here
-        profilePage: { results: [] },
-        popularProfiles: { results: [] },
-    });
-    const { popularProfiles } = profileData;
-    const currentUser = useCurrentUser();
-
-    useEffect(() => {
-        const handleMount = async () => {
-            try {
-                const { data } = await axiosReq.get(
-                    "profiles/?ordering=-followers_count"
-                );
-                setProfileData((prevState) => ({
-                    ...prevState,
-                    popularProfiles: data,
-                }));
-            } catch (err) {
-                console.log(err);
-            }
-        };
-        handleMount();
-    }, [currentUser]);
 
     return (
         <Container
