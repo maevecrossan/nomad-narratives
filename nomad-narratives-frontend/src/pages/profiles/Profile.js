745ee06868a761f67f634dc4b2f6340c6682ee80
Maeve Crossan <maevecrossan@gmail.com>
2025-03-23 14:47:10 +0000

    Allow user profiles and avatars to be displayed in popular profiles section.

--- nomad-narratives-frontend/src/pages/profiles/Profile.js
+++ nomad-narratives-frontend/src/pages/profiles/Profile.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import styles from '../../styles/Profile.module.css';
 import btnStyles from '../../styles/Button.module.css';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import { Link } from 'react-router-dom';
+import Avatar from '../../components/Avatar';
 
-const Profile = () => {
+const Profile = (props) => {
+    const {profile, mobile, imageSize=55} = props;
+    const {id, following_id, image, owner} = profile;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+
     return (
-        <div>Profile</div>
+        <div className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}>
+            <div>
+                <Link className="align-self-center" to={`/profiles/${id}`}>
+                    <Avatar src={image} height={imageSize} />
+                </Link>
+            </div>
+            <div className={`mx-2 ${styles.Wordbreak}`}>
+                <strong>
+                    {owner}
+                </strong>
+            </div>
+        </div>
     )
 }
 
