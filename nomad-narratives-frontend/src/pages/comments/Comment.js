b4ce2b84d43d76c43cf3066c833303fac998ad59
Maeve Crossan <maevecrossan@gmail.com>
2025-03-23 08:06:20 +0000

    Pass props to commenteditform component.

--- nomad-narratives-frontend/src/pages/comments/Comment.js
+++ nomad-narratives-frontend/src/pages/comments/Comment.js
@@ -55,7 +55,18 @@ const Comment = (props) => {
                 <Media.Body className="align-self-center ml-2">
                     <span className={styles.Owner}>{owner}</span>
                     <span className={styles.Date}>{updated_at}</span>
-                    {showEditForm ? <CommentEditForm /> : <p>{content}</p>}
+                    {showEditForm ? (
+                        <CommentEditForm
+                            id={id}
+                            profile_id={profile_id}
+                            content={content}
+                            profileImage={profile_image}
+                            setComments={setComments}
+                            setShowEditForm={setShowEditForm}
+                        />
+                    ) : (
+                        <p>{content}</p>
+                    )}
                 </Media.Body>
                 {is_owner && !showEditForm && (
                     <MoreDropdown
