aa94c1806cf698003c14011a76f4baa253f98ce8
Maeve Crossan <maevecrossan@gmail.com>
2025-03-24 17:43:32 +0000

    Adjust function import method.

--- nomad-narratives-frontend/src/pages/posts/TripPostPage.js
+++ nomad-narratives-frontend/src/pages/posts/TripPostPage.js
@@ -14,7 +14,7 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 import InfiniteScroll from "react-infinite-scroll-component";
 import Asset from "../../components/Asset";
-import fetchMoreData from "../../utils/utils";
+import { fetchMoreData } from "../../utils/utils";
 import PopularProfiles from "../profiles/PopularProfiles";
 
 function TripPostPage() {
