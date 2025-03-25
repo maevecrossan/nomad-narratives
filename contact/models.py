2362a2efdabbbe9c8f7ec4d9156512e7771fde66
Maeve Crossan <maevecrossan@gmail.com>
2025-03-25 13:24:01 +0000

    Create contact app and add model fields.

---
+++ contact/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Contact(models.Model):
+    '''
+    Fields for form to contact admin. 
+    '''
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
