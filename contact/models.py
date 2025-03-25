629669446a51f2cea2722e00a5f4f63561606e10
Maeve Crossan <maevecrossan@gmail.com>
2025-03-25 13:43:02 +0000

    Adjust name fields to just be one field.

--- contact/models.py
+++ contact/models.py
@@ -3,13 +3,12 @@ from django.db import models
 
 class Contact(models.Model):
     '''
-    Fields for form to contact admin. 
+    Fields for form to contact admin.
     '''
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    sender_name = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.sender_name}"
