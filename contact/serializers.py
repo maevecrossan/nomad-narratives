e0ba19fe7666001516c865eda4eb9cafc8ef7de0
Maeve Crossan <maevecrossan@gmail.com>
2025-03-27 07:45:32 +0000

    Add module and class docstring.

--- contact/serializers.py
+++ contact/serializers.py
@@ -1,3 +1,7 @@
+'''
+Serializer for the Contact model.
+'''
+
 from rest_framework import serializers
 from .models import Contact
 
@@ -7,5 +11,8 @@ class ContactSerializer(serializers.ModelSerializer):
     Serializer for contact model.
     '''
     class Meta:
+        '''
+        Returns all fields.
+        '''
         model = Contact
         fields = '__all__'
