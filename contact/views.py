881a3d3c1d261256763fa3c725d8e3c40fc5c179
Maeve Crossan <maevecrossan@gmail.com>
2025-03-27 07:48:26 +0000

    Add module docstring and pylint comment.

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,7 @@
+'''
+Defines the ContactCreateView, which provides an API endpoint
+for creating Contact model instances.
+'''
 from rest_framework import generics
 from .models import Contact
 from .serializers import ContactSerializer
@@ -7,5 +11,5 @@ class ContactCreateView(generics.CreateAPIView):
     '''
     View for contact model.
     '''
-    queryset = Contact.objects.all()
+    queryset = Contact.objects.all()  # pylint: disable=no-member
     serializer_class = ContactSerializer
