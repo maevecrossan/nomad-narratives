2a1202a3bb413f4558f11e837d3e706a2b22aa53
Maeve Crossan <maevecrossan@gmail.com>
2025-03-27 07:39:56 +0000

    Add module and class docstrings.

--- contact/apps.py
+++ contact/apps.py
@@ -1,6 +1,14 @@
+'''
+App configuration for the Contact application.
+'''
+
 from django.apps import AppConfig
 
 
 class ContactConfig(AppConfig):
+    '''
+    Configuration class for the Contact app.
+    Sets the default auto field type and specifies the app's name.
+    '''
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'contact'
