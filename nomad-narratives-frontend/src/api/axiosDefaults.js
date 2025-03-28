830b36f903a04dad17ab4921709a58dd47f51366
Maeve Crossan <maevecrossan@gmail.com>
2025-03-28 06:47:46 +0000

    Add missing colon and global process comment.

--- nomad-narratives-frontend/src/api/axiosDefaults.js
+++ nomad-narratives-frontend/src/api/axiosDefaults.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000"; /* global process */
 
 axios.defaults.baseURL = `${API_URL}/api/`;
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
