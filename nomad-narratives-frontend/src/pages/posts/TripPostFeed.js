c6491c03a5caf7688f416a5dc905e446772bc58d
Maeve Crossan <maevecrossan@gmail.com>
2025-03-29 08:40:49 +0000

    Comment out console log in catch block.

--- nomad-narratives-frontend/src/pages/posts/TripPostFeed.js
+++ nomad-narratives-frontend/src/pages/posts/TripPostFeed.js
@@ -33,7 +33,7 @@ function TripPostFeed({message, filter=""}) {
             setTripPosts(data);
             setHasLoaded(true);
           } catch (err) {
-            console.log(err);
+            // console.log(err);
           }
         };
 
