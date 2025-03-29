efcbe305ce5a87c1745a8c2fdb9303744e852f5f
Maeve Crossan <maevecrossan@gmail.com>
2025-03-29 07:45:04 +0000

    Import and call 'shouldRefreshToken' and 'removeTokenTimestamp' functions.

--- nomad-narratives-frontend/src/contexts/CurrentUserContext.js
+++ nomad-narratives-frontend/src/contexts/CurrentUserContext.js
@@ -7,6 +7,7 @@ import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { useHistory } from "react-router";
+import { removeTokenTimestamp, shouldRefreshToken } from "../utils/utils";
 
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
@@ -34,16 +35,19 @@ export const CurrentUserProvider = ({ children }) => {
     useMemo(() => {
         axiosReq.interceptors.request.use(
             async (config) => {
-                try {
-                    await axios.post("dj-rest-auth/token/refresh/");
-                } catch (err) {
-                    setCurrentUser((prevCurrentUser) => {
-                        if (prevCurrentUser) {
-                            history.push("/sign-in");
-                        }
-                        return null;
-                    });
-                    return config;
+                if (shouldRefreshToken()){
+                    try {
+                        await axios.post("dj-rest-auth/token/refresh/");
+                    } catch (err) {
+                        setCurrentUser((prevCurrentUser) => {
+                            if (prevCurrentUser) {
+                                history.push("/sign-in");
+                            }
+                            return null;
+                        });
+                        removeTokenTimestamp();
+                        return config;
+                    }
                 }
                 return config;
             },
@@ -65,6 +69,7 @@ export const CurrentUserProvider = ({ children }) => {
                             }
                             return null;
                         });
+                        removeTokenTimestamp();
                     }
                     return axios(err.config);
                 }
